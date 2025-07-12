e27858c40b80715306fc025ad5953fa43829f139
patel vandan <vandan11patel@gmail.com>
2025-07-12 10:29:30 +0530

    login page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
-
+import Login from "./login";
 export default function Home() {
   return (
     <>
-     <p className="f">hello</p>
+     {/* <p className="f">hello</p> */}
+     <Login/>
     </>
   );
 }
